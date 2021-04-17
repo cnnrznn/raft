a2b7981e5bb7dda930cea7ac286fe7b70ea2a1b6
Connor Zanin <czanin@newstore.com>
2021-04-16 19:34:25 -0600

    A basic HTTP server for a raft API

--- cmd/httpraft/main.go
+++ cmd/httpraft/main.go
@@ -3,15 +3,24 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 	"strconv"
+	"time"
 
 	"github.com/cnnrznn/raft"
 )
 
 type config struct {
 	Peers []string `json:"peers"`
+	APIs  []string `json:"apis"`
 	Self  int      `json:"self"`
+}
+
+type Server struct {
+	raft   *raft.Raft
+	config *config
 }
 
 func main() {
@@ -26,14 +35,36 @@ func main() {
 	fmt.Println(raft)
 
 	// Run raft
-	raft.Run()
+	go raft.Run()
+
+	// continuously print commited log
+	go func() {
+		for {
+			time.Sleep(5 * time.Second)
+			log := []string{}
+			entries := raft.Retrieve(0)
+			for _, entry := range entries {
+				log = append(log, entry.Msg)
+			}
+			fmt.Println(log)
+		}
+	}()
+
+	server := Server{
+		raft:   raft,
+		config: config,
+	}
 
 	// Open http endpoint and take input from users
 	// POST /request
+	http.HandleFunc("/request", server.handleRequest)
 
 	// Allow the user to query for log entries
 	// GET /log?index=<int>
 	// returns log starting at the provided index
+	http.HandleFunc("/log", server.handleLog)
+
+	log.Fatal(http.ListenAndServe(config.APIs[id], nil))
 }
 
 func readPeers() *config {
@@ -47,4 +78,52 @@ func readPeers() *config {
 	json.Unmarshal(bytes, &result)
 
 	return &result
+}
+
+type Request struct {
+	Entry string `json:"entry"`
+}
+
+type Response struct {
+	Success bool   `json:"success"`
+	Leader  string `json:"leader"`
+}
+
+type LogRequest struct {
+	Index int `json:"index"`
+}
+
+func (s *Server) handleRequest(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Must POST", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// Parse request
+	var req Request
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, "Problem parsing", http.StatusInternalServerError)
+		return
+	}
+
+	// Submit to raft
+	result := s.raft.Request(req.Entry)
+	resp := Response{
+		Success: result.Success,
+		Leader:  s.config.APIs[result.Leader],
+	}
+
+	// Write response
+	js, err := json.Marshal(resp)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
+
+func (s *Server) handleLog(w http.ResponseWriter, r *http.Request) {
 }
